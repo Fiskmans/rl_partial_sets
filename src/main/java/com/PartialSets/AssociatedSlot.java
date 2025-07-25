924ddaf6cb0b0da7ebd65d47f135c2ed521b6aeb
Måns Zivkovic Andersen <mans.andersen@massive.se>
2025-07-25 10:21:23 +0200

    Associated item no longer produce a warning if they're the only parts equipped

--- src/main/java/com/PartialSets/AssociatedSlot.java
+++ src/main/java/com/PartialSets/AssociatedSlot.java
@@ -8,16 +8,28 @@ public class AssociatedSlot extends ItemSlot
     AssociatedSlot(PartialSetsConfig aConfig, Integer... aItems)
     {
         super(aConfig, aItems);
-        //NonWarningAlternatives(aItems);
     }
 
     @Override
     EquipStatus IsEquipped(ItemContainer aEquipment, ItemManager aManager)
     {
-        if (myConfig.Associated())
-            return super.IsEquipped(aEquipment, aManager);
+        if (!myConfig.Associated())
+        {
+            return EquipStatus.NotRequired;
+        }
 
-        return EquipStatus.NotRequired;
+        switch (super.IsEquipped(aEquipment, aManager))
+        {
+            case Equipped:
+            case EquippedNoWarning:
+                return EquipStatus.EquippedNoWarning;
+            case NotPresent:
+                return EquipStatus.NotPresent;
+            case NotRequired:
+                return EquipStatus.NotRequired;
+        }
+
+        return EquipStatus.NotPresent;
     }
 
     @Override
